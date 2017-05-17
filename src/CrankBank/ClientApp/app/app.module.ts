4f9e93f2bbe66cbff81c8129b4ef5e3f3fbddb99
Ranu <O@O.net>
2017-05-17 10:34:11 +0100

    Add Account Module and replace it with default home page

--- src/CrankBank/ClientApp/app/app.module.ts
+++ src/CrankBank/ClientApp/app/app.module.ts
@@ -7,7 +7,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
 import { AppComponent } from './components/app/app.component'
-import { HomeComponent } from './components/home/home.component';
+import { AccountListComponent } from './components/account/account-list/account-list.component';
+import { AccountSummaryComponent } from './components/account/account-summary/account-summary.component';
+import { AccountDetailComponent } from './components/account/account-detail/account-detail.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 
 // The NG module decorator, which is a specific type of function that allows us to attach metadata to our app module
@@ -15,15 +17,17 @@ import { HeaderComponent } from './components/shared/header/header.component';
     bootstrap: [ AppComponent ], // This tells us that we're gonna be launching our app component as our Startup Component
     declarations: [ // This is a list of the components that we're gonna reference inside our module
         AppComponent,
-        HomeComponent,
+        AccountListComponent,
+        AccountDetailComponent,
+        AccountSummaryComponent,
         HeaderComponent
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
         RouterModule.forRoot([ // Path Configuration that set up the Angular Routing
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: '**', redirectTo: 'home' }
+            { path: '', redirectTo: 'account', pathMatch: 'full' },
+            { path: 'account', component: AccountListComponent },
+            { path: '**', redirectTo: 'account' }
         ])
     ]
 })
---
+++ src/CrankBank/ClientApp/app/components/account/account-summary/account-summary.component.ts
@@ -0,0 +1,15 @@
+﻿
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'account-summary',
+    templateUrl: './account-summary.component.html',
+    styleUrls: ['./account-summary.component.css']
+})
+export class AccountSummaryComponent {
+    constructor() {
+    }
+
+}
+    
